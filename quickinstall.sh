a8426b8d1b13a63cac5d420acf5ccf10ea5e0907
ipat8 <ppatrick828@gmail.com>
2018-10-01 12:04:44 -0400

    Fix path

--- quickinstall.sh
+++ quickinstall.sh
@@ -4,7 +4,7 @@ systemctl enable rgm.service
 
 curl https://raw.githubusercontent.com/ipat8/Intro-To-STEM-RGM/master/rgm.py -o /etc/poly/rgm.py
 curl https://raw.githubusercontent.com/ipat8/Intro-To-STEM-RGM/master/rgm.sh -o /etc/poly/rgm.sh
-curl https://raw.githubusercontent.com/ipat8/Intro-To-STEM-RGM/master/complete.sh -o /etc/complete.sh
+curl https://raw.githubusercontent.com/ipat8/Intro-To-STEM-RGM/master/complete.sh -o /etc/poly/complete.sh
 curl https://raw.githubusercontent.com/ipat8/Intro-To-STEM-RGM/master/sss.mp3 -o /etc/poly/sss.mp3
 chmod +x /etc/poly/rgm.sh
 chmod +x /etc/poly/complete.sh
